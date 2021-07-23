4d5e0a4c5bb1559c6d4979a5264f150e0ccaa0d3
Manindra1024 <70622352+Manindra1024@users.noreply.github.com>
2021-07-23 15:52:53 +0530

    Update main.tf

--- Challenge#1/modules/delpoapp/main.tf
+++ Challenge#1/modules/delpoapp/main.tf
@@ -1,1 +1,44 @@
 
+data "aws_ami" "web" {
+  
+  owners = ["*************"] # Canonical
+}
+data "aws_ami" "app" {
+  
+  owners = ["*************"] # Canonical
+}
+
+data "aws_ami" "db" {
+  
+  owners = ["*************"] # Canonical
+}
+
+# can be delpoyedon public subnet
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  
+  tags = {
+    Name = "test"
+  }
+}
+
+# can be delpoyedon private subnet
+resource "aws_instance" "app" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "test"
+  }
+}
+
+# can be delpoyedon dmz subnet
+resource "aws_instance" "db" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "test"
+  }
+}
