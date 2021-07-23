94dc321a86ca1654af4965f490c2a26c725ddc7b
Manindra1024 <70622352+Manindra1024@users.noreply.github.com>
2021-07-23 15:48:22 +0530

    Update main.tf

--- Challenge#1/main.tf
+++ Challenge#1/main.tf
@@ -1,1 +1,16 @@
 
+module "vpc" {
+  source = "modules/vpc"
+  
+  vpc_name = "test vpc"
+  cidr =
+  region =
+}
+  
+module "deployapp" {
+  source = "modules/deployapp"
+  
+  vpc_name = "test vpc"
+  cidr =
+  region =
+}  
